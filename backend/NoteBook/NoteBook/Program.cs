bb51912474f934fc2c0033f8a2e4e9dc58eefba9
DESKTOP-9LJG4SO\AB <batyhtik3@gmail.com>
2024-06-30 13:40:58 +0300

    added data access layer for backend

--- backend/NoteBook/NoteBook/Program.cs
+++ backend/NoteBook/NoteBook/Program.cs
@@ -7,16 +7,12 @@ namespace NoteBook
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -24,10 +20,7 @@ namespace NoteBook
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
